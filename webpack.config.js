29b281dbfa72ea7fd7d0ea26398a9f7768bbb084
최은철 <wndqhrgkdlel@gmail.com>
2023-12-06 11:56:38 +0900

    엔트리 설정을 변경해줘야 함

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js", // 엔트리 포인트 설정
+  entry: "./src/scripts/main.js",
     output: {
         path: path.resolve(__dirname, "dist"), // 아웃풋 디렉토리 설정
         filename: "bundle.js", // 번들링된 파일의 이름 설정
