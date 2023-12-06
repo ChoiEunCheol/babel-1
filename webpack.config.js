c14a022fbf287e088b70eeee8b2fd64dfcdfb894
최은철 <wndqhrgkdlel@gmail.com>
2023-12-06 11:40:56 +0900

    css파일 관련 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"), // 아웃풋 디렉토리 설정
         filename: "bundle.js", // 번들링된 파일의 이름 설정
-      }
-}
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/, // .css 확장자를 가진 파일을 찾기
+          use: ["style-loader", "css-loader"], // 순서 주의: 뒤에서부터 앞으로 로더가 실행됨
+        },
+      ],
+    },
+  };
